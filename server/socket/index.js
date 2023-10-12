a1ac8ac89bd3fe0c5835fcbb1edf4d6f2268b64a
CarlosDahmen <carlos.dahmen@gmail.com>
2023-10-12 10:07:03 -0400

    Separate Client

--- server/socket/index.js
+++
@@ -1,7 +0,0 @@
-export const socket = (socket) => {
-  console.log(`user ${socket.id} connected`);
-
-  socket.on("disconnect", () => {
-    console.log(`user ${socket.id} disconnected`);
-  });
-};
